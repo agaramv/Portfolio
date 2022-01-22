ae3dd56c82bd17b5e28ec4aac4b52dc2b48a412a
agaramv <42156415+agaramv@users.noreply.github.com>
2022-01-22 16:19:43 -0500

    Finished Portfolio Structure
    Still have to customize for me in some areas

--- vaPortfolio/src/components/Footer/Footer.js
+++ vaPortfolio/src/components/Footer/Footer.js
@@ -6,9 +6,36 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:804-937-6566"></LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:vagaram09@gmail.com"></LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Turning Paper into Paper Airplanes</Slogan>
+        </CompanyContainer>
+        <SocialIconsContainer>
+          <SocialIcons href="https://github.com">
+            <AiFillGithub size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://linkedin.com">
+            <AiFillLinkedin size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://instagram.com">
+            <AiFillInstagram size="3rem" />
+          </SocialIcons>
+        </SocialIconsContainer>
+
+      </SocialIconsContainer>
+
+    </FooterWrapper>
   );
 };
 
--- vaPortfolio/src/components/Technologies/Technologies.js
+++ vaPortfolio/src/components/Technologies/Technologies.js
@@ -1,12 +1,45 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { SiAngularjs, SiSpring, SiGithub } from 'react-icons/si';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id="#tech">
+    <SectionDivider />
+    <br />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>I've worked with a range of technologies in the web development world from frontend to backend. </SectionText>
+    <List>
+      <ListItem>
+        <SiAngularjs size="3rem"/>
+        <ListContainer>
+          <ListTitle>Frontend</ListTitle>
+          <ListParagraph>Experience with <br />
+            Angular
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <SiSpring size="3rem"/>
+        <ListContainer>
+          <ListTitle>Backend</ListTitle>
+          <ListParagraph>Experience with <br />
+            Spring Framework and MySQL
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <SiGithub size="3rem"/>
+        <ListContainer>
+          <ListTitle>Tech Tools</ListTitle>
+          <ListParagraph>Experience with <br />
+            Github and Postman
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+    <SectionDivider/>
+  </Section>
 );
 
 export default Technologies;
