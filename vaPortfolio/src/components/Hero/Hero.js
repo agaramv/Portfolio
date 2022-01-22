1e79158bbdeb1f16f89d49623a95cb01f8d0a1eb
agaramv <42156415+agaramv@users.noreply.github.com>
2022-01-22 14:31:32 -0500

    Made hero section

--- vaPortfolio/src/components/Hero/Hero.js
+++ vaPortfolio/src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Person Portfolio
+      </SectionTitle>
+      <SectionText>
+        I am a second year computer science major and data science minor at the University of Virginia who works on software for companies to expand their businesses. Reach out to me for any job positions. Actively Looking. 
+      </SectionText>
+      <Button onClick={()=> window.location = 'https://google.com'}>Hire Me</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
