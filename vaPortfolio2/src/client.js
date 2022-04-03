286a9371bd749bfd880bd930a678b2543f79b02e
agaramv <42156415+agaramv@users.noreply.github.com>
2022-04-03 19:23:55 -0400

    Installed necessary packages

--- vaPortfolio2/src/client.js
+++ vaPortfolio2/src/client.js
@@ -2,11 +2,11 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  projectId: 'hgf6d30z',
   dataset: 'production',
   apiVersion: '2022-02-01',
   useCdn: true,
-  token: process.env.REACT_APP_SANITY_TOKEN,
+  token: 'sk3Ce0WwyQkUKSwIp02rFzDMuMXKjD00EEJk3Vi2LW3VNd47Jun6ARYPCAOFPpBA5OkRdj7xGrweg18zZGMTUltFyYnAPMJSw8Eo2uRLo9poAlNZjZpdVHae3BFKOLvU3Yt3VmR3d0UGCS8Vjo4hvYqiKGML8bVfk3PfCc0kSvRltaYP31PZ',
 });
 
 const builder = imageUrlBuilder(client);
